7ff8c1defa3bffd895e3598b69793c0ffe8c200c
Shinichi Haramizu <shinichi@haramizu.jp>
2021-05-14 14:20:43 +0900

    update dynamic route

---
+++ components/date.js
@@ -0,0 +1,6 @@
+import { parseISO, format } from 'date-fns'
+
+export default function Date({ dateString }) {
+  const date = parseISO(dateString)
+  return <time dateTime={dateString}>{format(date, 'yyyy 年 M 月 d 日')}</time>
+}

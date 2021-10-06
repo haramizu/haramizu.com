47105b200add9a24f7ba25e676b28cfbd7f714ba
Shinichi Haramizu <shinichi@haramizu.jp>
2021-10-06 16:38:38 +0900

    test google tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -79,6 +79,15 @@ class MyDocument extends Document {
               gtag('config', 'G-TYBMDRMJLX');`,
             }}
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MQSXXN7');`,
+            }}
+          />
         </Head>
         <body className="antialiased text-black bg-white dark:bg-gray-900 dark:text-white">
           <Main />

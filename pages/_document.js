c2e129f1bc69b8ac0c60f4bf8af4ccd227954e1e
Shinichi Haramizu <shinichi@haramizu.jp>
2022-01-28 14:36:21 +0900

    add mootrack

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,13 @@ class MyDocument extends Document {
         <Head>
           <script
             dangerouslySetInnerHTML={{
+              __html: `!function(t,n,e,o,a){function d(t){var n=~~(Date.now()/3e5),o=document.createElement(e);o.async=!0,o.src=t+"?ts="+n;var a=document.getElementsByTagName(e)[0];a.parentNode.insertBefore(o,a)}t.MooTrackerObject=a,t[a]=t[a]||function(){return t[a].q?void t[a].q.push(arguments):void(t[a].q=[arguments])},window.attachEvent?window.attachEvent("onload",d.bind(this,o)):window.addEventListener("load",d.bind(this,o),!1)}(window,document,"script","//cdn.stat-track.com/statics/moosend-tracking.min.js","mootrack");
+              //tracker has to be initialized otherwise it will generate warnings and wont sendtracking events
+              mootrack('init', '0850722f-08b9-473d-9568-9f4a2241ea90');`,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=

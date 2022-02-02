7595a2b079eef3a23b30d8271aff6dd34707da0c
Shinichi Haramizu <shinichi@haramizu.jp>
2022-02-02 10:22:09 +0900

    update new version

---
+++ data/authors/default.md
@@ -0,0 +1,22 @@
+---
+name: Shinichi Haramizu
+avatar: /static/images/haramizu.jpg
+occupation: Manager, Sales Engeering Japan
+company: Sitecore Japan
+email: shinichi@haramizu.jp
+twitter: https://twitter.com/haramizu
+linkedin: https://www.linkedin.com/haramizu
+github: https://github.com/haramizu
+---
+
+デジタルマーケティング基盤となる Sitecore の製品を取り上げています。主な製品はこんな形です。
+
+* Sitecore Experience Platform 
+* Sitecore Content Hub 
+* Sitecore CDP
+* Sitecore OrderCloud 
+* Sitecore Send 
+
+上記のサービスに関する Tips をまとめています。
+
+このサイトは Next.js で構築しており、 Tailwind Nextjs Starter Blog - https://github.com/timlrx/tailwind-nextjs-starter-blog をベースにカスタマイズをして運用しています。
--- pages/_document.js
+++ pages/_document.js
@@ -2,24 +2,8 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="ja">
+      <Html lang="en" className="scroll-smooth">
         <Head>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `!function(t,n,e,o,a){function d(t){var n=~~(Date.now()/3e5),o=document.createElement(e);o.async=!0,o.src=t+"?ts="+n;var a=document.getElementsByTagName(e)[0];a.parentNode.insertBefore(o,a)}t.MooTrackerObject=a,t[a]=t[a]||function(){return t[a].q?void t[a].q.push(arguments):void(t[a].q=[arguments])},window.attachEvent?window.attachEvent("onload",d.bind(this,o)):window.addEventListener("load",d.bind(this,o),!1)}(window,document,"script","//cdn.stat-track.com/statics/moosend-tracking.min.js","mootrack");
-              //tracker has to be initialized otherwise it will generate warnings and wont sendtracking events
-              mootrack('init', '0850722f-08b9-473d-9568-9f4a2241ea90');`,
-            }}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-MQSXXN7');`,
-            }}
-          />
           <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
           <link
             rel="icon"
@@ -37,48 +21,15 @@ class MyDocument extends Document {
           <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" content="#000000" />
-          <link rel="alternate" type="application/rss+xml" href="/rss.xml" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            rel="preload"
-            href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/fonts/KaTeX_Main-Regular.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
-            href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/fonts/KaTeX_Math-Italic.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
-            href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/fonts/KaTeX_Size2-Regular.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="preload"
-            href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/fonts/KaTeX_Size4-Regular.woff2"
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
+          <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
           <link
             rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/katex@0.13.0/dist/katex.min.css"
-            integrity="sha384-t5CR+zwDAROtph0PXGte6ia8heboACF9R5l/DiY+WZ3P2lxNgvJkQk5n7GPvLMYw"
+            href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css"
+            integrity="sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc"
             crossOrigin="anonymous"
           />
         </Head>
-        <body className="antialiased text-black bg-white dark:bg-gray-900 dark:text-white">
+        <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
           <Main />
           <NextScript />
         </body>

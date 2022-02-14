ebd7b6e95bd8ed9191a53adefdf508c382cf78e9
Shinichi Haramizu <shinichi@haramizu.jp>
2022-02-14 16:55:36 +0900

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,17 @@ class MyDocument extends Document {
     return (
       <Html lang="en" className="scroll-smooth">
         <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer', 'GTM-MQSXXN7');
+              `,
+            }}
+          />
           <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
           <link
             rel="icon"
@@ -30,6 +41,14 @@ class MyDocument extends Document {
           />
         </Head>
         <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=GTM-MQSXXN7`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
           <Main />
           <NextScript />
         </body>

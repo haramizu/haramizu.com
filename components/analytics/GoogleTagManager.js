b0b135599cb3af10a14dbf84443ed6c5c28410f2
Shinichi Haramizu <shinichi@haramizu.jp>
2022-02-16 11:41:55 +0900

    Update GoogleTagManager.js

--- components/analytics/GoogleTagManager.js
+++ components/analytics/GoogleTagManager.js
@@ -1,17 +1,23 @@
 import Script from 'next/script'
 
+import siteMetadata from '@/data/siteMetadata'
+
 const GTMScript = () => {
   return (
     <>
-      <Script strategy="lazyOnload" id="gtm-script">
-        {`
+      <Script
+        id="gtm"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-MQSXXN7');
-        `}
-      </Script>
+          })(window,document,'script','dataLayer','${siteMetadata.analytics.googleAnalyticsId}');
+          `,
+        }}
+      />
     </>
   )
 }

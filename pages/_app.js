20a1474b72f4c3ed8b78b33783f2318c1e370c82
Shinichi Haramizu <shinichi@haramizu.jp>
2022-02-17 15:08:22 +0900

    update gtm

--- pages/_app.js
+++ pages/_app.js
@@ -13,12 +13,13 @@ import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
 
 import { useEffect } from 'react'
+import { Router, useRouter } from 'next/router'
+import Script from 'next/script'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
 import { GTM_ID, pageview } from '../lib/gtm'
-import { Router, useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
   const router = useRouter()
@@ -34,6 +35,19 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
+      <Script
+        strategy="afterInteractive"
+        id="googletag"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+        }}
+      />
       {isDevelopment && isSocket && <ClientReload />}
       <Analytics />
       <LayoutWrapper>
